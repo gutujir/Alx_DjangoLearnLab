3040c20ad7f9985e162425931c09c0d39323cec5
Gutu <gutujirex@gmail.com>
2025-09-07 20:57:07 +0300

    permission required fixed

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.views.generic import DetailView
 from .models import Book, Library, Author
 
